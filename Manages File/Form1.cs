c62005af9e13207513a14c7be64e319dc73db3a1
Diego Alza <pxdalza@cibertec.edu.pe>
2022-06-28 02:35:17 +0000

    Crear y leer archivos

--- Manages File/Form1.cs
+++ Manages File/Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,6 +13,9 @@ namespace Manages_File
 {
     public partial class Form1 : Form
     {
+        private String path = @"../../files";
+        private String path_file = @"../../files/archivo.xyz";
+
         public Form1()
         {
             InitializeComponent();
@@ -20,15 +24,49 @@ namespace Manages_File
         private void Form1_Load(object sender, EventArgs e)
         {
 
+            if (!Directory.Exists(path))
+            {
+                Directory.CreateDirectory(path);
+            }
+            else
+            {
+
+                if (File.Exists(path_file))
+                {
+                    using (StreamReader file = new StreamReader(path_file))
+                    {
+                        String line;
+                        while ((line = file.ReadLine()) != null)
+                        {
+                            txtMessage.Text += line + Environment.NewLine;
+                        }
+
+                        file.Close();
+                    }
+                }
+
+            }
+
         }
 
         private void btnGuardar_Click(object sender, EventArgs e)
         {
 
-            //crear algoritmo para guardar archivo.
-            var num = 3;
-            MessageBox.Show(num.ToString());
+            if (!Directory.Exists(path))
+            {
+                Directory.CreateDirectory(path);
+            }
+            else
+            {
 
+                if (File.Exists(path_file))
+                {
+                    File.Delete(path_file);
+                }
+
+                File.WriteAllText(path_file, txtMessage.Text);
+
+            }
         }
     }
 }
