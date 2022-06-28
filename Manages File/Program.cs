6de940e0b8bfa475cad14df098d3d32299637f90
pxdalza <67033179+pxdalza@users.noreply.github.com>
2022-06-28 02:48:36 +0000

    prueba

--- Manages File/Program.cs
+++ Manages File/Program.cs
@@ -14,9 +14,11 @@ namespace Manages_File
         [STAThread]
         static void Main()
         {
+
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Form1());
+            
         }
     }
 }
